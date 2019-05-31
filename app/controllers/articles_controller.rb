3e6f1fddc2354ada1fa7ba04535bc8e5813308ba
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-05-31 10:49:12 +0900

    修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,25 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-  # GET /articles
-  # GET /articles.json
+
   def index
     @articles = Article.all
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
+
   def show
   end
 
-  # GET /articles/new
+
   def new
     @article = Article.new
   end
 
-  # GET /articles/1/edit
+
   def edit
   end
 
-  # POST /articles
-  # POST /articles.json
+
   def create
     @article = Article.new(article_params)
 
@@ -37,8 +34,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
+
   def update
     respond_to do |format|
       if @article.update(article_params)
